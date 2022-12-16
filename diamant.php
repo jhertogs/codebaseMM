9184531871bffef5ee65c7c40d4f1c3d8356967d
jhertogs <97103567@st.deltion.nl>
2022-12-16 12:34:14 +0100

    rkghwriegbeu

--- diamant.php
+++ diamant.php
@@ -1,5 +1,15 @@
 <?php 
+$omvang=readline("voer een omvang in voor je diamant:");
 
+for($i=0; $i < $omvang; $i++){
+    echo $omvang;
+    for($a=0; $a < $omvang; $a++) {
+        echo $omvang. "\n";
+    }
+    
+        
+    
+}
 
 
 
