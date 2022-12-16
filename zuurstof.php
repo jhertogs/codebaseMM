ba007125ec2fde9ba218300ac3baefdbaec1407c
jhertogs <97103567@st.deltion.nl>
2022-12-16 10:47:23 +0100

    finished (denk ik)

--- zuurstof.php
+++ zuurstof.php
@@ -6,7 +6,7 @@ $drukbar= readline();
 echo"Voer nu de inhoud in liters van de fles in: ";
 $inhoudl= readline();
 //if statement checkd of de 2 input's numerisch en groter dan 0 zijn
-if(is_numeric($drukbar) && is_numeric($inhoudl) && $inhoudl < 0 && $drukbar < 0) {
+if(is_numeric($drukbar) && is_numeric($inhoudl) && $inhoudl > 0 && $drukbar > 0) {
     //liter zuurstof in de fles word berekend
     $lO2fles= $drukbar * $inhoudl;
     echo"De hoeveelheid zuurstof in de fles is: $lO2fles\n";
@@ -14,11 +14,11 @@ if(is_numeric($drukbar) && is_numeric($inhoudl) && $inhoudl < 0 && $drukbar < 0)
     echo"Voer de benodigde hoeveelheid liter zuurstof per minuut in: ";
     $lpm= readline();
     //if statement checkd of input numerisch is en hoger is dan 0
-    if(is_numeric($lpm) && $lpm < 0) {
+    if(is_numeric($lpm) && $lpm > 0) {
         $lxbar= $lO2fles * $drukbar;
         $minutenO2= $lxbar / $lpm;
         echo"Je hebt $minutenO2 minuut/minuten zuurstof";
-
+        exit;
 }}
 //als de conditie in de if statements false is
 echo"INVALIDE INPUT VOER ALLEEN GETALLEN IN!\n";
