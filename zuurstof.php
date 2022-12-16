98a8ec307a2c75a957096fdc79369ab40523dd5b
jhertogs <97103567@st.deltion.nl>
2022-12-16 09:58:11 +0100

    calculations

--- zuurstof.php
+++ zuurstof.php
@@ -1,13 +1,18 @@
 <?php 
 
+
 echo"Hallo voer de druk van de fles in bar in: ";
-$drukbar= readine();
+$drukbar= readline();
 echo"Voer nu de inhoud in liters van de fles in: ";
 $inhoudl= readline();
-$lo2fles= $drukbar * $inhoudl;
-echo"De hoeveelheid zuurstof in de fles is: $lo2fles";
+$lO2fles= $drukbar * $inhoudl;
+echo"De hoeveelheid zuurstof in de fles is: $lO2fles\n";
 
-
-
+echo"Voer de benodigde hoeveelheid liter zuurstof per minuut in: ";
+$lpm= readline();
+$lxbar= $lO2fles * $drukbar;
+$minutenO2= $lxbar / $lpm;
+echo"Je hebt $minutenO2 minuut/minuten zuurstof";
+exit;
 
 ?>
