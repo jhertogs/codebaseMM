d422f2664860679c529c960440524359475046f5
jhertogs <97103567@st.deltion.nl>
2022-12-16 09:47:01 +0100

    echo's and readlines
    and a calculation

---
+++ zuurstof.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo"Hallo voer de druk van de fles in bar in: ";
+$drukbar= readine();
+echo"Voer nu de inhoud in liters van de fles in: ";
+$inhoudl= readline();
+$lo2fles= $drukbar * $inhoudl;
+echo"De hoeveelheid zuurstof in de fles is: $lo2fles";
+
+
+
+
+?>
